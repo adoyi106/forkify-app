2c4d02fd50db0039c08bea0f8fdf5bda224b6f95
adoyi <adoyiowoicho@gmail.com>
2023-05-12 10:08:04 +0100

    first commit

---
+++ src/js/controller.js
@@ -0,0 +1,81 @@
+import * as model from './model.js';
+import searchView from './view/searchView.js';
+import recipeView from './view/recipeView.js';
+import resultsView from './view/resultsView.js';
+import paginationView from './view/paginationView.js';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const recipeContainer = document.querySelector('.recipe');
+// if (module.hot) {
+//   module.hot.accept();
+// }
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+const controlShowRecipe = async function () {
+  //Rendering spinner
+  recipeView.renderSpinner();
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+    // console.log(id);
+    //1.) fetch recipe
+    // renderSpinner(recipeContainer);
+    recipeView.renderSpinner();
+
+    await model.loadRecipe(id);
+    // const {recipe} =  model.state;
+
+    //2. Render recipe to screen.
+    recipeView.render(model.state.recipe);
+    //TEST
+    controlServings();
+  } catch (err) {
+    // console.error(err);
+    recipeView.renderError();
+  }
+};
+const controlSearchResults = async function () {
+  try {
+    //1.) get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+    resultsView.renderSpinner();
+    //2.) Load search results
+
+    await model.loadSearchResults(query);
+    //3. resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchPage(3));
+    //4. render initial pagination buttons
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.error(`${err} you know`);
+  }
+};
+const controlPagination = function (goToPage) {
+  //1. render new results
+  resultsView.render(model.getSearchPage(goToPage));
+  //4. render new pagination buttons
+  paginationView.render(model.state.search);
+  console.log('ya');
+};
+
+const controlServings = function (newServings) {
+  //update recipe servings
+  model.updateServings(newServings);
+  //render new recipe view
+  recipeView.render(model.state.recipe);
+};
+
+// showRecipe();
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
+const init = function () {
+  recipeView.addHandlerRender(controlShowRecipe);
+  recipeView.addHandlerUpdateServings(controlServings);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+};
+init();
---
+++ src/js/model.js
@@ -0,0 +1,74 @@
+import { async } from 'regenerator-runtime';
+import { API_URL } from './config.js';
+import { RES_PER_PAGE } from './config.js';
+import { getJSON } from './helpers.js';
+
+export const state = {
+  recipe: {},
+  search: {
+    query: '',
+    results: [],
+    page: 1,
+    resPerPage: RES_PER_PAGE,
+  },
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}${id}`);
+
+    const { recipe } = data.data;
+    console.log(recipe);
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      ingredients: recipe.ingredients,
+      imageUrl: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      sourceUrl: recipe.source_url,
+    };
+    console.log(state.recipe);
+    console.log(state.recipe.servings);
+  } catch (err) {
+    // console.error(`${err} 💥💥`);
+    throw err;
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    if (!query) return;
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        imageUrl: rec.image_url,
+      };
+    });
+    console.log(data);
+  } catch (err) {
+    console.error(`${err} 💥💥`);
+    throw err;
+  }
+};
+
+export const getSearchPage = function (page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * state.search.resPerPage;
+  const end = page * state.search.resPerPage;
+  return state.search.results.slice(start, end);
+};
+export const updateServings = function (newServings) {
+  state.recipe.ingredients.forEach(ing => {
+    ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
+    //newQty = oldQty * newQty/oldServings
+  });
+  state.recipe.servings = newServings;
+  // console.log(state.recipe.servings);
+};
